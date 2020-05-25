43ff06d72b8d239bbbc065bcd890dacafdc50e83
Gitesh Goyal <gitesh0799@gmail.com>
2020-05-25 21:06:14 +0530

    Update cachematrix.R

--- cachematrix.R
+++ cachematrix.R
@@ -1,10 +1,14 @@
 ## Put comments here that give an overall description of what your
 ## functions do
 
-## Write a short comment describing this function
+## This special object is a list of 4 functions:
+## 1. set(y) -> Receives a matrix as a parameter and saves it inside the object.
+## 2. get() -> Returns the matrix saved inside the object. 
+## 3. setinverse(inverse) -> Receives a matrix and saves it inside the object (cache).
+## 4. getinverse() -> Returns the inverse of the matrix if it's saved inside the object (cache).
 
 makeCacheMatrix <- function(x = matrix()) {
-         i <- NULL
+  i <- NULL
   set <- function(y) {
     x <<- y
     i <<- NULL
@@ -18,11 +22,17 @@ makeCacheMatrix <- function(x = matrix()) {
 }
 
 
-## Write a short comment describing this function
+## The cacheSolve function returns the inverse of the special matrix object inside the makeCache function.
+##
+## If the inverse of the matrix is already stored in the object as a cache, cacheSolve() prints a message
+## specifying it's getting cached data and quickly returns it.
+##
+## If, however, the inverse of the matrix isn't stored in the object, the function solve() is utilized to calculate
+## the inverse of the matrix inside x, being promptly cached inside the object. cacheSolve() then returns the inverse
+## of the matrix.
 
 cacheSolve <- function(x, ...) {
-        ## Return a matrix that is the inverse of 'x'
-          i <- x$getinverse()
+  i <- x$getinverse()
   if (!is.null(i)) {
     message("getting cached data")
     return(i)
